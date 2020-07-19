73492a4020709684b2eca036284265a2446604d4
Yannick Bijl <yannick.bijl@gmail.com>
2020-07-19 16:13:21 +0200

    Updated ProteinMassEstimator
    Moved to a code centric folder
    Added table with amino acids and their weights.
    Added calculation of the protein weight. (Only estimation)

--- MassEstimator/ProteinMassEstimator.cr
+++ MassEstimator/ProteinMassEstimator.cr
@@ -1,7 +1,14 @@
 require "option_parser"
 
-sequence = ""
+aa_weights = {'A'=>71.0788, 'R'=>156.1875, 'N'=>114.1038,
+              'D'=>115.0886, 'C'=>103.1388, 'E'=>129.1155,
+              'Q'=>128.1307, 'G'=>57.0519, 'H'=>137.1411,
+              'I'=>113.1594, 'L'=>113.1594, 'K'=>128.1741,
+              'M'=>131.1926, 'F'=>147.1766, 'P'=>97.1167,
+              'S'=>87.0782, 'T'=>101.1051, 'W'=>186.2132,
+              'Y'=>163.1760, 'V'=>99.1326}
 
+sequence = ""
 OptionParser.parse do |parser|
   parser.banner = "Welcome to ProteinMassEstimator"
 
@@ -28,6 +35,10 @@ end
 
 puts "Protein: #{sequence}"
 
+
+weight = 0
 sequence.chars.each do |aa|
-  puts "AminoAcid: #{aa}"
+  weight += aa_weights[aa]
 end
+
+puts "Weight: #{weight}"
